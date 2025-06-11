31bacce95681fb05aaf83ef3c7fe362cc329507e
alexcrist <alexecrist@gmail.com>
2025-06-11 15:53:29 -0700

    Update stripe checkout

--- web-backend/api/createCheckoutSession.ts
+++ web-backend/api/createCheckoutSession.ts
@@ -2,14 +2,32 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import Stripe from "stripe";
 import { getStripe } from "../util/stripe";
 
+interface Item {
+  priceId: string;
+  quantity: string;
+}
+
 const stripe = getStripe();
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { items } = req.body;
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  let items: Item[];
+  try {
+    const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+    items = body.items;
+  } catch (err) {
+    console.error(err);
+    console.error("req.body", req.body);
+    return res.status(400).json({ error: "Invalid JSON" });
+  }
 
   // Verify that there is enough inventory of each product being purchased
   const lineItems: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
-  for (const { priceId, quantity } of items) {
+  for (const { priceId, quantity: rawQuantity } of items) {
+    const quantity = parseInt(rawQuantity);
     const price = await stripe.prices.retrieve(priceId, {
       expand: ["product"],
     });
